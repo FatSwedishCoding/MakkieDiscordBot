c78825fae6631d5f651c221e67d1f714296c6259
marcusf1993 <marcusf1993@hotmail.com>
2020-01-20 04:24:49 +0100

    first upload public

---
+++ __resource.lua
@@ -0,0 +1,21 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'Discord Bot' 			-- Resource Description
+
+--server_script {						-- Server Scripts
+	--'Config.lua',
+	--'SERVER/Server.lua',
+	--'@mysql-async/lib/MySQL.lua',
+--}
+
+server_script { 
+     '@mysql-async/lib/MySQL.lua',
+     'SERVER/Server.lua'
+ }
+
+client_script {						-- Client Scripts
+	'Config.lua',
+	'CLIENT/Weapons.lua',
+	'CLIENT/Client.lua',
+}
+
